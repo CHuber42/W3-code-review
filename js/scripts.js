4dcfadcdafe3a14ac24c45455f6684bdff493ca5
Christopher Huber <CHuber42.work@gmail.com>
2020-04-03 09:48:32 -0700

    code refactoring to replace redundant variables in specs 7/8/9

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function transform(numberInputAsInt) {
   var range = []
-  console.log(numberInputAsInt)
+
   for (i = 0; i <= numberInputAsInt; i++){
     range.push(i);
   }
@@ -8,31 +8,26 @@ function transform(numberInputAsInt) {
   for (i = 0; i < range.length; i++) {
     var temparray = (range[i]).toString().split("");   //convert an entry to string and split it (for more-than-1-digit numbers)
     temparray.sort();                                  //sort the components lowest-to-highest (51 becomes "1" "5")
-    var flagResult = ""                                    //holds this index's post-int-check result ("one", "two", "three", or range[i]; ex: "459")
+                                       //holds this index's post-int-check result ("one", "two", "three", or range[i]; ex: "459")
 
     for (j = 0; j < temparray.length; j++) {         //loop/switch to perform the above and set the flag ("one", "two", "three", or range[i]; ex: "459")
       switch (temparray[j]) {
         case "1":
-          flagResult = "one";
+          range[i] = "Beep!";
           break
         case "2":
-          flagResult = "two";
+          range[i] = "Boop!";
           break;
         case "3":
-          flagResult = "three";
-          break;
-        default:
-          if (flagResult === ""){                   //Don't set the "non-"1""2""3"" flag unless "1""2""3" flag has not been set
-            flagResult = range[i].toString();
-          }
+          range[i] = "Won't you be my neighbor?";
           break;
       }
 
     }
-    range[i] = flagResult;  
+     
   }
-  console.log(range)
-
+  
+console.log(range)
   
 
 }
